1318d55cacf8e5fb690c5abf10cbf3bcf025fc9a
napoli (HiroakiTanaka) <hi.k.tanaka@mercari.com>
2020-05-23 14:56:03 +0900

    add cors setting (stable)

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	cloud.google.com/go/bigquery v1.4.0 // indirect
 	cloud.google.com/go/logging v1.0.0
 	github.com/go-chi/chi v4.0.3+incompatible
+	github.com/go-chi/cors v1.1.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	"github.com/docup/docup-api/env"
 	"github.com/docup/docup-api/log"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/oauth2/jws"
 	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2beta3"
@@ -72,6 +73,20 @@ func main() {
 	}
 
 	r := chi.NewRouter()
+	
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	
 	// routing
 	{
 		//
