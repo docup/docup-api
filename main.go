8469dcb67e21e69615979654ceb37e6621acf5cf
napoli (HiroakiTanaka) <hi.k.tanaka@mercari.com>
2020-09-03 20:33:05 +0900

    wip

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -17,11 +16,12 @@ import (
 	"github.com/docup/docup-api/graph"
 	"github.com/docup/docup-api/graph/generated"
 	"github.com/docup/docup-api/log2"
+	"github.com/go-chi/chi"
 	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 )
 
-const defaultPort = "8081"
+const defaultPort = "8080"
 
 func main() {
 	ctx := context.Background()
@@ -47,6 +47,11 @@ func main() {
 		AllowCredentials: true,
 	})
 
+	tokenVerifier, err := NewFirebaseAuthTokenVerifier(ctx, "docup-269111")
+	if err != nil {
+		stdlog.Fatalf(err.Error())
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -64,21 +69,45 @@ func main() {
 			},
 		},
 	})
-	srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
-		res := next(ctx)
-		return res
-	})
-	srv.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
-		res, err = next(ctx)
-		return res, err
-	})
-	srv.AroundResponses(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
-		return nil
+	//srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+	//	res := next(ctx)
+	//	return res
+	//})
+	//srv.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
+	//	res, err = next(ctx)
+	//	return res, err
+	//})
+	//srv.AroundResponses(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+	//	res := next(ctx)
+	//	return res
+	//})
+
+	r := chi.NewRouter()
+
+	// protected routes
+	r.Group(func(r chi.Router) {
+		r.Use(func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				token, err := tokenVerifier.Verify(ctx, r)
+				if err != nil {
+					http.Error(w, http.StatusText(401), 401)
+					w.Write([]byte(err.Error()))
+					return
+				}
+				newContext := context.WithValue(r.Context(), "GoogleIDToken", token)
+				next.ServeHTTP(w, r.WithContext(newContext))
+			})
+		})
+
+		r.Handle("/private-query", c.Handler(srv))
 	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", c.Handler(srv))
+	// public routes
+	r.Group(func(r chi.Router) {
+		r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+		r.Handle("/query", c.Handler(srv))
+	})
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
