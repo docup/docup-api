a7aec499ec39a00c26d41ee466743995b4e9d40a
napoli (HiroakiTanaka) <hi.k.tanaka@mercari.com>
2020-09-03 21:21:02 +0900

    wip

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,19 +5,69 @@ package graph
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"strconv"
 
 	"github.com/docup/docup-api/graph/generated"
 	"github.com/docup/docup-api/graph/model"
+	"google.golang.org/api/iterator"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	_, _, err := r.Firestore.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada" + strconv.FormatUint(rand.Uint64(), 10),
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	todos := []*model.Todo{}
+	iter := r.Firestore.Collection("users").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		a := struct {
+			First string `json:"first"`
+		}{}
 
-	return nil, nil
+		dd := doc.Data()
+		MapToStruct(dd, &a)
+
+		todos = append(todos, &model.Todo{
+			Text: fmt.Sprintf("%+v", a.First),
+		})
+	}
+	return todos, nil
+}
+
+func MapToStruct(mapVal map[string]interface{}, val interface{}) error {
+	//structVal := reflect.Indirect(reflect.ValueOf(val))
+	//for name, elem := range mapVal {
+	//	v := structVal.FieldByName(name)
+	//
+	//	if v.IsValid() {
+	//		v.Set(reflect.ValueOf(elem))
+	//	}
+	//}
+	//return
+
+	bin, err := json.Marshal(mapVal)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bin, val)
 }
 
 // Mutation returns generated.MutationResolver implementation.
